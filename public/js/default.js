467bb762b9cae7eb24ebb6ef7f14e89d01859fc3
Bob Faist <bob.faist@gmail.com>
2011-08-30 08:51:14 -0400

    footer style, Searching message on ajax call

--- public/js/default.js
+++ public/js/default.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $("a").click(function() {
+    $('#loading').hide().ajaxStart(function(){$(this).show();}).ajaxStop(function(){$(this).hide();});
+
+    $(".search_term_item a").click(function() {
          var link_id = $(this).attr('id');
          var search_term = link_id.split(':');
          $.get('/ajax/' + search_term[0] + '/' + search_term[1], function(data) {
